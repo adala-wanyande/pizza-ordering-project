465b99347cc7325766e6b234c45344671d663d24
TEDDY <teddy.mbugua@student.moringaschool.com>
2023-02-20 19:13:20 +0300

    first debugging

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
-import React from "React";
-import './App.css';
+import React from "react";
+import NavigationBar from './NavigationBar'
 
-function About() {
+const About = () => {
   return (
+    <>
+    <NavigationBar active="Contact"></NavigationBar>
     <div className="About ">
  <iframe className='map'
              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8474606867426!2d36.82201841483971!3d-1.2640029990780584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f171ff7af4bdd%3A0x9dbde3a5d6365341!2sPizza%20Inn!5e0!3m2!1sen!2ske!4v1676895300677!5m2!1sen!2ske" 
@@ -56,6 +58,7 @@ function About() {
                 </div>
             </div>
     </div>
+    </>
   );
 }
 
