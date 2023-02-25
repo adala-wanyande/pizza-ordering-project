1f836df020db7e13f1a51aeaa8f5a876ee8df87c
benard-dev <benardwanyande@gmail.com>
2023-02-25 14:42:57 +0300

    Portfolio worthy

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,19 @@ const About = () => {
   return (
     <>
       <NavigationBar active="About"></NavigationBar>
+      <>
+        <div className="about frame">
+          <div style={{ backgroundColor: "white", height: "auto", margin: "40px 100px 40px 100px" , borderRadius: "5px"  }}>
+            <h1 style={{ paddingLeft:"30px", marginTop:"10px", color: "#A6B49A"}}>About Us</h1>
+            <h2 style={{ paddingLeft:"30px", fontSize: "32px" }}>Our branches</h2>
+            <div className="text-muted" style={{ padding: "10px 30px 10px 30px", lineHeight:"1.8" }}><small>
+            <p>Welcome to Rose's Gourmet Pizza Restaurant, a fully fledged restaurant specializing in delivering a relaxed and memorable dining experience. We're currently in 12 locations; 4 in Nairobi and 8 in Kampala.</p>
+            <p>We're growing and may soon open a location closer to you. We look forward to serving you.</p>
+            <p>Rose's Gourmet Pizza Restaurant is part of <a href="https://github.com/benard-dev">©benard-dev</a> group of companies, the parent company to many other companies.</p>
+            </small></div>
+          </div>
+        </div>
+      </>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import NavigationBar from './NavigationBar'
+import '../index.css'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
   return (
     <>
       <NavigationBar active="Home"></NavigationBar>
+      <>
+        <div className="frame frame-1">
+          <div style={{ width: "550px", marginTop: "5%", marginLeft: "5%"}}>
+            <h1 className="h1-frame-1"><span style={{ color: "red" }}>More</span> Than Just Pizza</h1>
+            <p className="h2-frame-1">Our menu has something for everyone. Stop by for a quick bite or get your favorites delivered right to your door.</p>
+          <div>
+          </div>
+            <Link to="/order">
+              <Button style={{ backgroundColor:"#E48041", borderColor: "#E48041", outlineColor:"#E48041", marginRight: "30px" }}>Order Now</Button>
+            </Link>
+            <Link to="/menu">
+              <Button className = "see-menu-button" style ={{ backgroundColor: "transparent", borderColor: "transparent", outlineColor:"transparent" }}>
+                See the Menu
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style={{ paddingLeft: "10px", width: "20px", height: "20px", fill: "white", marginBottom: "2px" }}><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>            </Button>
+            </Link>
+          </div>
+      </div> 
+      </>
     </>
   )
 }
